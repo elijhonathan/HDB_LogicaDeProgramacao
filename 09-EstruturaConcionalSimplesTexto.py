9ec7a16a68b0bbaeb6ebb0ee4ab6c71c1ae9049d
elijhonathan <eligrancej@hotmail.com>
2024-04-11 17:41:53 -0300

    ok

---
+++ 09-EstruturaConcionalSimplesTexto.py
@@ -0,0 +1,11 @@
+print("Responda apenas sim ou não!")
+
+resposta = input("O seu time ganhou a ultima partida? ")
+
+if ((resposta == "sim") or (resposta == "Sim") or (resposta == "SIM")):
+    print("Que legal!")
+    print("Você deve estar feliz!")
+
+if ((resposta == "não") or (resposta == "nao") or (resposta == "NÃO") or (resposta == "NAO") or (resposta == "Não")):
+    print("Que pena!")
+    print("Você deve estar chateado")
